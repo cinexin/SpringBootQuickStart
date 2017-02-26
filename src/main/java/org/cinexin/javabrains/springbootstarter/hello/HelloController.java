4e3f3bc052abb6e7f154b4a57edd5ee7ba8f1364
cinexin <migueguillamon@gmail.com>
2017-02-26 18:03:48 +0100

    Hello World controller

---
+++ src/main/java/org/cinexin/javabrains/springbootstarter/hello/HelloController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.cinexin.javabrains.springbootstarter.hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author migue
+ *
+ * Spring Controller "REST"
+ * We want to return a "Hello" message when user access /hello URL
+ */
+@RestController
+public class HelloController {
+
+	@RequestMapping("hello")
+	public String sayHi() {
+		return "Hello World!";
+	}
+}
