c8b71c09a1052012036386e48a1c66f60bf950d8
cinexin <migueguillamon@gmail.com>
2017-02-26 12:35:16 +0100

    Some refactoring and dummy main class created

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Following gr8 Spring Boot Tutorial from JavaBrains: https://www.youtube.com/playlist?list=PLmbC-xnvykcghOSOJ1ZF6ja3aOgZAgaMO
 
 MADE WITH SPRING TOOL SUITE: http://spring.io/tools/sts
+
+To run it from Spring Tool Suite (STS), just rigth-click on "CourseApiApp" class "Run AS.. > Java Application". A servlet container will be started using 8080 port by default
+
---
+++ src/main/java/org/cinexin/javabrains/springbootstarter/CourseApiApp.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package org.cinexin.javabrains.springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author migue
+ *
+ */
+@SpringBootApplication
+public class CourseApiApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(CourseApiApp.class, args);
+	}
+
+}
